b161fb1bb233e86a66155fa11138764b6163ec8a
lw020806 <lw020806@sjtu.edu.cn>
2022-08-05 13:32:04 +0800

    modify test_formal.sh

--- test_formal.sh
+++ test_formal.sh
@@ -1,13 +1,13 @@
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
-	path="/root/data/server_0715/"
+	path="/root/data/server_"
 	targetIP="${path}/targets/20x200/${i}_200.ips"
 	outputFile="${path}/shanghai/20x200/${i}_output.yrps"
 
-	minttl=6
-	maxttl=25
-	rate=4000
+	minttl=#6
+	maxttl=#25
+	rate=#4000
 
-	sudo ./yarrp -o ${outputFile}  -t ICMP  -r ${rate} -i ${targetIP}  -l ${minttl}  -m ${maxttl}
+	sudo ./yarrp -o ${outputFile}  -t UDP -r ${rate} -i ${targetIP}  -l ${minttl}  -m ${maxttl} -C
 	sleep 1m
 done
